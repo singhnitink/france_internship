e0988edc0ec7967790ac3db250b5af27058cd2ee
Nitin Kumar Singh <72007459+singhnitink@users.noreply.github.com>
2023-09-14 15:32:32 +0200

    rename_beads.sh

--- rename_beads.sh
+++ rename_beads.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-#This file is used to rename beads
-#CAB - CA;BB - C;OP - O;NP - N
+# This file is used to rename beads
+# CAB - CA; BB - C; OP - O; NP - N
 
-#bash rename_beads.sh filename.gro
+# Usage: bash rename_beads.sh filename.gro
 
 # Define the replacements
 replacements=(
-  's/CAB/ CA/g'
-  's/BB/ C/g'
-  's/OP/ O/g'
-  's/NP/ N/g'
+  's/\bCAB\b/ CA/g'
+  's/\bBB\b/ C/g'
+  's/\bOP\b/ O/g'
+  's/\bNP\b/ N/g'
 )
 
 # Loop through the replacements and apply them to the file
 for replacement in "${replacements[@]}"; do
-  sed -i "$replacement" $1
+  sed -i "$replacement" "$1"
 done
 
 echo "Replacements completed."
